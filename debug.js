d1c293cbc8c8aac079bc43ec986cd9a0dff4fe23
WesleyChia14 <wchia@unitedvirtualities.com>
2020-01-14 19:56:45 -0600

    nodeat  scale

---
+++ debug.js
@@ -0,0 +1,16 @@
+const debugHttpIncoming = require('debug')('http:incoming')
+const debugHttpOutgoing = require('debug')('http:outgoing')
+
+let outgoingRequest = {
+  url: 'https://risingstack.com'
+}
+
+// sending some request
+debugHttpOutgoing('sending request to %s', outgoingRequest.url)
+
+let incomingRequest = {
+  body: '{"status": "ok"}'
+}
+
+// serving some request
+debugHttpOutgoing('got JSON body %s', incomingRequest.body)
